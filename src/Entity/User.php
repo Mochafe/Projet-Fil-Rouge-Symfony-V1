e02a6cd97e0eba578aa9232ab981f9532b35b21e
Florian Pasquet <florianpasquett@gmail.com>
2022-11-25 09:31:40 +0100

    profil modif

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cette adresse e-mail')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
 
