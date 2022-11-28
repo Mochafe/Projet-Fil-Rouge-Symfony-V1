40c6d64b6c0f41e842ca58393fc2228c5fcaafc7
Florian Pasquet <florianpasquett@gmail.com>
2022-11-28 15:29:14 +0100

    cart update

--- public/js/cartUpdate.js
+++ public/js/cartUpdate.js
@@ -1,9 +1,28 @@
 const cartItems = document.getElementsByName("quantity");
 const prices = document.getElementsByName("prices");
+const subtotal = document.getElementById("subtotal");
+const vat = document.getElementById("vat-price");
+const total = document.getElementById("total");
+
 
 cartItems.forEach((cartItem, index) => {
+    updateCalculation(index, cartItem);
     cartItem.addEventListener("change", () => {
-        prices[index].innerHTML = `${cartItem.value * cartItem.dataset.price} €`;
+        updateCalculation(index, cartItem);
         fetch(`/cart/update?cartDetail=${cartItem.dataset.id}&quantity=${cartItem.value}`);
     });
 });
+
+function updateCalculation(index, cartItem) {
+    let sum = 0;
+    cartItems.forEach(cartItem => {
+        sum += Number(cartItem.dataset.price * cartItem.value);
+    });
+
+    console.log(sum);
+
+    prices[index].innerHTML = `${cartItem.value * cartItem.dataset.price} €`;
+    subtotal.innerText = sum.toFixed(2);
+    vat.innerHTML = (sum * (vat.dataset.vat / 100)).toFixed(2);
+    total.innerHTML = (sum * ((vat.dataset.vat / 100) + 1) + 4.99).toFixed(2);
+}
