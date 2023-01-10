f6bba604652077fcabd79afdfc113d176202706b
Florian Pasquet <florianpasquett@gmail.com>
2023-01-10 15:09:35 +0100

    service update 🐛

--- src/Service/CategoryService.php
+++ src/Service/CategoryService.php
@@ -18,17 +18,25 @@ class CategoryService {
         $this->productRepository = $productRepository;
         $this->imageRepository = $imageRepository;
     }
+    
+    //TODO main cat can't delete
     public function delete($id) {
         //get noCat
         $noCat = $this->categoryRepository->find(1);
         $category = $this->categoryRepository->find($id);
 
         $products = $category->getProducts();
-
         //set product category to noCat
-        foreach($products as $index => $product) {
+        foreach($products as $product) {
             $product->setCategory($noCat);
             $this->productRepository->save($product);
+        }
+
+        $childs = $category->getChilds();
+
+        foreach($childs as $child) {
+            $child->setParent($noCat);
+            $this->categoryRepository->save($child);
         }
 
         $this->imageRepository->remove($category->getImage());
